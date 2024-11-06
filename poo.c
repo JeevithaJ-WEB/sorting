bdc079ab41239146c9bc770a62d82488f6d8de7b
Prathibha520 <mprathibha871@gmail.com>
2024-11-06 16:50:14 +0530

    Update and rename README.md to poo.c
    add heap

---
+++ poo.c
@@ -0,0 +1,10 @@
+void heapSort(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+    
+    for (int i = n - 1; i > 0; i--) {
+        swap(&arr[0], &arr[i]);
+        heapify(arr, i, 0);
+    }
+}
